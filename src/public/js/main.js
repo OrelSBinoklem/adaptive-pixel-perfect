e619388408dffa92ab562eb1e11eb4ef1e60f370
Evgeniy <robingoode2@mail.ru>
2017-04-13 05:30:44 +0300

    Тест веток (изучаю гит) #2

--- src/public/js/main.js
+++ src/public/js/main.js
@@ -93,7 +93,6 @@ jQuery(function($) {
         /*Синхронизация по группам сессий*/
         /****************************************************/
         (function() {
-            
             //Свернуть-развернуть список выбора групп
             $(".shab__btn-open-groups-session").on("click", function () {
                 if(!$(this).hasClass('active')) {
